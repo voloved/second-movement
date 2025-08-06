bee488a6243ce1e348b1afcc22118951214d8362
David Volovskiy <devolov@gmail.com>
2025-08-06 08:06:15 -0400

    Merge branch 'wordle_port' into devolov

--- watch-faces.mk
+++ watch-faces.mk
@@ -3,6 +3,7 @@ SRCS += \
   ./watch-faces/clock/beats_face.c \
   ./watch-faces/clock/world_clock_face.c \
   ./watch-faces/clock/mars_time_face.c \
+  ./watch-faces/clock/ish_face.c \
   ./watch-faces/complication/alarm_face.c \
   ./watch-faces/complication/advanced_alarm_face.c \
   ./watch-faces/complication/countdown_face.c \
@@ -11,6 +12,8 @@ SRCS += \
   ./watch-faces/complication/sunrise_sunset_face.c \
   ./watch-faces/complication/moon_phase_face.c \
   ./watch-faces/complication/days_since_face.c \
+  ./watch-faces/complication/breathing_face.c \
+  ./watch-faces/complication/squash_face.c \
   ./watch-faces/complication/totp_face.c \
   ./watch-faces/demo/all_segments_face.c \
   ./watch-faces/demo/character_set_face.c \
@@ -32,4 +35,9 @@ SRCS += \
   ./legacy/watch_faces/complication/festival_schedule_face.c \
   ./legacy/watch_faces/complication/endless_runner_face.c \
   ./legacy/watch_faces/complication/wordle_face.c \
+  ./watch-faces/complication/probability_face.c \
+  ./watch-faces/clock/close_enough_face.c \
+  ./watch-faces/complication/tarot_face.c \
+  ./watch-faces/complication/kitchen_conversions_face.c \
+  ./watch-faces/complication/periodic_table_face.c \
 # New watch faces go above this line.

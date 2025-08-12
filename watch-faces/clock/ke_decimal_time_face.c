b0d1b7af3d9c9eeb07a3a625448dae6bd297667e
David Volovskiy <devolov@gmail.com>
2025-08-11 21:21:41 -0400

    Merge branch 'main' into devolov

--- watch-faces/clock/ke_decimal_time_face.c
+++ watch-faces/clock/ke_decimal_time_face.c
@@ -1,7 +1,7 @@
 /*
  * MIT License
  *
- * Copyright (c) <#year#> <#author_name#>
+ * Copyright (c) 2025 Joey Castillo
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -24,33 +24,77 @@
 
 #include <stdlib.h>
 #include <string.h>
-#include "<#watch_face_name#>_face.h"
+#include "ke_decimal_time_face.h"
+#include "watch_utility.h"
 
-void <#watch_face_name#>_face_setup(uint8_t watch_face_index, void ** context_ptr) {
+static void _display_date(watch_date_time_t date_time) {
+    char buf[3];
+
+    watch_display_text_with_fallback(WATCH_POSITION_TOP_LEFT, watch_utility_get_long_weekday(date_time), watch_utility_get_weekday(date_time));
+    snprintf(buf, sizeof(buf), "%2d", date_time.unit.day);
+    watch_display_text(WATCH_POSITION_TOP_RIGHT, buf);
+}
+
+static void _display_time(ke_decimal_time_state_t *state, watch_date_time_t date_time, bool low_energy) {
+    char buf[8];
+    uint32_t value = date_time.unit.hour * 3600 + date_time.unit.minute * 60 + date_time.unit.second;
+
+    if (value == state->previous_time) return;
+
+    value = value * 100;
+    value = value / 864;
+    snprintf(buf, sizeof(buf), "%04ld#o", value);
+
+    // if under 10%, display 0.00 instead of 00.00
+    if (value < 1000) buf[0] = ' ';
+
+    // if low energy, truncate at the tens place
+    if (low_energy) buf[3] = 0;
+
+    watch_display_text(WATCH_POSITION_BOTTOM, buf);
+
+    state->previous_time = value;
+}
+
+void ke_decimal_time_face_setup(uint8_t watch_face_index, void ** context_ptr) {
     (void) watch_face_index;
     if (*context_ptr == NULL) {
-        *context_ptr = malloc(sizeof(<#watch_face_name#>_state_t));
-        memset(*context_ptr, 0, sizeof(<#watch_face_name#>_state_t));
+        *context_ptr = malloc(sizeof(ke_decimal_time_state_t));
+        memset(*context_ptr, 0, sizeof(ke_decimal_time_state_t));
         // Do any one-time tasks in here; the inside of this conditional happens only at boot.
     }
     // Do any pin or peripheral setup here; this will be called whenever the watch wakes from deep sleep.
 }
 
-void <#watch_face_name#>_face_activate(void *context) {
-    <#watch_face_name#>_state_t *state = (<#watch_face_name#>_state_t *)context;
+void ke_decimal_time_face_activate(void *context) {
+    ke_decimal_time_state_t *state = (ke_decimal_time_state_t *)context;
 
-    // Handle any tasks related to your watch face coming on screen.
+    if (watch_sleep_animation_is_running()) {
+        watch_stop_sleep_animation();
+    }
+
+    // force re-display of date and time in EVENT_ACTIVATE
+    state->previous_day = 0xFF;
+    state->previous_time = 0xFFFFFFFF;
 }
 
-bool <#watch_face_name#>_face_loop(movement_event_t event, void *context) {
-    <#watch_face_name#>_state_t *state = (<#watch_face_name#>_state_t *)context;
+bool ke_decimal_time_face_loop(movement_event_t event, void *context) {
+    ke_decimal_time_state_t *state = (ke_decimal_time_state_t *)context;
+    watch_date_time_t date_time = movement_get_local_date_time();
 
     switch (event.event_type) {
         case EVENT_ACTIVATE:
-            // Show your initial UI here.
-            break;
+            watch_set_decimal_if_available();
+            if (movement_alarm_enabled()) {
+                watch_set_indicator(WATCH_INDICATOR_SIGNAL);
+            }
+            // fall through
         case EVENT_TICK:
-            // If needed, update your display here.
+            _display_time(state, date_time, false);
+            if (state->previous_day != date_time.unit.day) {
+                _display_date(date_time);
+                state->previous_day = date_time.unit.day;
+            }
             break;
         case EVENT_LIGHT_BUTTON_UP:
             // You can use the Light button for your own purposes. Note that by default, Movement will also
@@ -69,7 +113,12 @@ bool <#watch_face_name#>_face_loop(movement_event_t event, void *context) {
             // If you did not resign in EVENT_TIMEOUT, you can use this event to update the display once a minute.
             // Avoid displaying fast-updating values like seconds, since the display won't update again for 60 seconds.
             // You should also consider starting the tick animation, to show the wearer that this is sleep mode:
-            // watch_start_sleep_animation(500);
+            if (!watch_sleep_animation_is_running()) {
+                watch_start_sleep_animation(500);
+                watch_display_text(WATCH_POSITION_SECONDS, "  ");
+                watch_display_text(WATCH_POSITION_MINUTES, "  ");
+            }
+            _display_time(state, date_time, true);
             break;
         default:
             // Movement's default loop handler will step in for any cases you don't handle above:
@@ -84,7 +133,7 @@ bool <#watch_face_name#>_face_loop(movement_event_t event, void *context) {
     return true;
 }
 
-void <#watch_face_name#>_face_resign(void *context) {
+void ke_decimal_time_face_resign(void *context) {
     (void) context;
 
     // handle any cleanup before your watch face goes off-screen.

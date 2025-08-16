95eebafe05ac51eeab4e05d7d5e0609bca5f24e7
David Volovskiy <devolov@gmail.com>
2025-08-16 11:58:38 -0400

    Modified I and B to diplay 1 or 8 if imperfect

--- watch-library/shared/watch/watch_common_display.c
+++ watch-library/shared/watch/watch_common_display.c
@@ -48,6 +48,8 @@ void watch_display_character(uint8_t character, uint8_t position) {
     if (watch_get_lcd_type() == WATCH_LCD_TYPE_CUSTOM) {
         if (character == 'R' && position > 1 && position < 8) character = 'r'; // We can't display uppercase R in these positions
         else if (character == 'T' && position > 1 && position < 8) character = 't'; // lowercase t is the only option for these positions
+        else if (character == 'B' && position > 1 && position < 8) character = '8';
+        else if (character == 'I' && position > 1 && position < 8) character = '1';
     } else {
         // special cases for positions 4 and 6
         if (position == 4 || position == 6) {

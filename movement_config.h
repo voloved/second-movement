1dd5ff5ff55783ee2ad00f461918bca1376fb915
David Volovskiy <devolov@gmail.com>
2025-08-23 12:19:03 -0400

    Added simon face

--- movement_config.h
+++ movement_config.h
@@ -47,6 +47,7 @@ const watch_face_t watch_faces[] = {
     wordle_face,
     higher_lower_game_face,
     lander_face,
+    simon_face,
     party_face,
     festival_schedule_face
 };
@@ -59,7 +60,7 @@ const watch_face_t watch_faces[] = {
  * Some folks also like to use this to hide the preferences and time set faces from the normal rotation.
  * If you don't want any faces to be excluded, set this to 0 and a long Mode press will have no effect.
  */
-#define MOVEMENT_TERIARY_FACE_INDEX (MOVEMENT_NUM_FACES - 6)
+#define MOVEMENT_TERIARY_FACE_INDEX (MOVEMENT_NUM_FACES - 7)
 #define MOVEMENT_SECONDARY_FACE_INDEX (MOVEMENT_TERIARY_FACE_INDEX - 5) // or (0)
 
 /* Custom hourly chime tune. Check movement_custom_signal_tunes.h for options. */

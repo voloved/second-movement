981c4346ef5090d231534584e391d30d04582c59
David Volovskiy <devolov@gmail.com>
2025-08-24 08:11:16 -0400

    Return to current branch once done

--- create-to-share.sh
+++ create-to-share.sh
@@ -2,6 +2,16 @@
 
 set -e  # Exit on error
 
+# Save current branch
+current_branch=$(git rev-parse --abbrev-ref HEAD)
+
+# Check for detached HEAD
+if [ "$current_branch" = "HEAD" ]; then
+    return_to_branch=""
+else
+    return_to_branch=$current_branch
+fi
+
 rm -rf release
 
 # Function to build and move firmware
@@ -12,19 +22,24 @@ build_and_move() {
 
     make clean
     make BOARD="$board" DISPLAY="$display" SHARE=true
-    mkdir -p release/$branch
+    mkdir -p "release/$branch"
     mv build/firmware.uf2 "release/$branch/firmware-${board}-${display}.uf2"
 }
 
 build_all_for_branch() {
     local branch="$1"
-    git checkout $branch
-    build_and_move sensorwatch_pro classic $branch
-    build_and_move sensorwatch_pro custom $branch
-    build_and_move sensorwatch_red custom $branch
-    build_and_move sensorwatch_red classic $branch
+    git checkout "$branch"
+    build_and_move sensorwatch_pro classic "$branch"
+    build_and_move sensorwatch_pro custom "$branch"
+    build_and_move sensorwatch_red  custom "$branch"
+    build_and_move sensorwatch_red  classic "$branch"
 }
 
 build_all_for_branch devolov
 build_all_for_branch devolov-nocounter32
+
+if [ -n "$return_to_branch" ]; then
+    git checkout "$return_to_branch"
+fi
+
 make clean

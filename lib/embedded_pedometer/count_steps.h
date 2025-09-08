0c4d735ea66d428fc9e9fde53c758072f2bcf109
David Volovskiy <devolov@gmail.com>
2025-09-08 19:57:44 -0400

    made step counter addition to lis2dw_monitor_face

--- lib/embedded_pedometer/count_steps.h
+++ lib/embedded_pedometer/count_steps.h
@@ -2,8 +2,8 @@
 #define count_steps_h
 #include "stdint.h"
 
-#define SAMPLING_RATE           20                       //20 hz sampling rate
-#define NUM_TUPLES              80                       //80 sets of accelerometer readings (so in other words, 80*3 = 240 samples)
+#define SAMPLING_RATE           25                       //25 hz sampling rate
+#define NUM_TUPLES              100                      //80 sets of accelerometer readings (so in other words, 100*3 = 300 samples)
 #define WINDOW_LENGTH           NUM_TUPLES/SAMPLING_RATE //window length in seconds
 
 uint8_t count_steps(int8_t *data);

7ea19bf193fe1e18dd75b09e5167ca5328877078
David Volovskiy <devolov@gmail.com>
2025-09-14 18:35:29 -0400

    step_count algorithm takes in magnitude directly; don't read step counter if the buffer is above 32

--- lib/embedded_pedometer/count_steps.h
+++ lib/embedded_pedometer/count_steps.h
@@ -1,11 +1,13 @@
 #ifndef count_steps_h
 #define count_steps_h
 #include "stdint.h"
+#include "lis2dw.h"
 
 #define COUNT_STEPS_SAMPLING_RATE    25                                               //25 hz sampling rate
 #define COUNT_STEPS_NUM_TUPLES       100                                              //80 sets of accelerometer readings (so in other words, 100*3 = 300 samples)
 #define COUNT_STEPS_WINDOW_LENGTH    COUNT_STEPS_NUM_TUPLES/COUNT_STEPS_SAMPLING_RATE //window length in seconds
 
-uint8_t count_steps(int8_t *data);
+uint32_t count_steps_approx_l2_norm(lis2dw_reading_t reading);
+uint8_t count_steps(uint8_t *mag_sqrt);
 
 #endif /* count_steps_h */

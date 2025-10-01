3d43f4c47eea0ad338fe26ac3db88db4a464c191
David Volovskiy <devolov@gmail.com>
2025-10-01 06:40:18 -0400

    Added step counter found in Bangle App

--- lib/embedded_pedometer/count_steps.h
+++ lib/embedded_pedometer/count_steps.h
@@ -10,7 +10,7 @@
 
 typedef struct {
     uint8_t count;
-    uint8_t magnitude[COUNT_STEPS_NUM_TUPLES];
+    int magnitude[COUNT_STEPS_NUM_TUPLES];
 } count_steps_magnitude_data_t;
 
 uint32_t count_steps_approx_l2_norm(lis2dw_reading_t reading);
@@ -18,4 +18,15 @@ uint8_t count_steps(uint8_t *mag_sqrt);
 uint8_t count_steps_simple(lis2dw_fifo_t *fifo_data);
 uint32_t get_steps_simple_threshold(void);
 
+/// Initialise step counting
+void stepcount_init(void);
+
+/* Registers a new data point for step counting. Data is expected
+ * as 12.5Hz, 8192=1g, and accMagSquared = sqrt(x*x + y*y + z*z)
+ *
+ * Returns the number of steps counted for this accel interval
+ */
+
+int stepcount_new(uint32_t accMag);
+int stepcount_new_from_fifo(lis2dw_fifo_t *fifo_data);
 #endif /* count_steps_h */

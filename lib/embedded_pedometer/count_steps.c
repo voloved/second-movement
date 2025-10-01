5d3f76da5825d25d7564d0798d2b1c4920871a23
David Volovskiy <devolov@gmail.com>
2025-10-01 07:29:02 -0400

    Removed unused step counter and added ability to turn on espruino step counter

--- lib/embedded_pedometer/count_steps.c
+++ lib/embedded_pedometer/count_steps.c
@@ -5,15 +5,6 @@
 
 #define DEBUG_PRINT false
 
-//this algorithm is a simple adaptation of the following paper:
-//"RecoFit - Using a Wearable Sensor to Find, Recognize, and Count Repetitive Exercises"
-
-#define NUM_AUTOCORR_LAGS       50          //number of lags to calculate for autocorrelation. 50 lags @25Hz corresponds to a step rate of 0.5Hz...its probably not physically possible to walk much slower than this
-#define DERIV_FILT_LEN          5           //length of derivative filter
-#define LPF_FILT_LEN            9           //length of FIR low pass filter
-#define AUTOCORR_DELTA_AMPLITUDE_THRESH 7e8 //this is the min delta between peak and trough of autocorrelation peak
-#define AUTOCORR_MIN_HALF_LEN   3           //this is the min number of points the autocorrelation peak should be on either side of the peak
-
 #define SIMPLE_THRESHOLD                15000  // Magnitudes at or above this threshold are considered a step, but can change if USE_WINDOW_AVG is true
 #define SIMPLE_THRESHOLD_MULT           1.5 // Multiplier for the moving average threshold adjustment. It was seen in some testing that 50% higher than the average worked well.
 #define SIMPLE_SAMP_IGNORE_STEP         3   // After detecting a step, ignore this many samples to avoid double counting
@@ -23,18 +14,6 @@
 #define MAX_FIFO_SIZE_SIMPLE            13
 #define MAX_SIMPLE_STEPS                (MAX_FIFO_SIZE_SIMPLE / SIMPLE_SAMP_IGNORE_STEP)
 
-static int8_t deriv_coeffs[DERIV_FILT_LEN]        = {-6,31,0,-31,6};            //coefficients of derivative filter from https://www.dsprelated.com/showarticle/814.php
-static int8_t lpf_coeffs[LPF_FILT_LEN]            = {-5,6,34,68,84,68,34,6,-5}; //coefficients of FIR low pass filter generated in matlab using FDATOOL
-static int32_t lpf[COUNT_STEPS_NUM_TUPLES]                    = {0};                        //hold the low pass filtered signal
-static int64_t autocorr_buff[NUM_AUTOCORR_LAGS]   = {0};                        //holds the autocorrelation results
-static int64_t deriv[NUM_AUTOCORR_LAGS]           = {0};                        //holds derivative
-
-static void derivative(int64_t *autocorr_buff, int64_t *deriv);
-static void autocorr(int32_t *lpf, int64_t *autocorr_buff);
-static void remove_mean(int32_t *lpf);
-static void lowpassfilt(uint8_t *mag_sqrt, int32_t *lpf);
-static uint8_t get_precise_peakind(int64_t *autocorr_buff, uint8_t peak_ind);
-static void get_autocorr_peak_stats(int64_t *autocorr_buff, uint8_t *neg_slope_count, int64_t *delta_amplitude_right, uint8_t *pos_slope_count, int64_t *delta_amplitude_left, uint8_t peak_ind);
 
 static uint32_t step_counter_threshold = SIMPLE_THRESHOLD;
 
@@ -58,204 +37,6 @@ uint32_t count_steps_approx_l2_norm(lis2dw_reading_t reading)
     return ax + ((15 * ay) >> 4) + ((3 * az) >> 3);
 }
 
-
-//take a look at the original autocorrelation signal at index i and see if
-//its a real peak or if its just a fake "noisy" peak corresponding to
-//non-walking. basically just count the number of points of the
-//autocorrelation peak to the right and left of the peak. this function gets
-//the number of points to the right and left of the peak, as well as the delta amplitude
-static void get_autocorr_peak_stats(int64_t *autocorr_buff, uint8_t *neg_slope_count, int64_t *delta_amplitude_right, uint8_t *pos_slope_count, int64_t *delta_amplitude_left, uint8_t peak_ind) {
-    
-    //first look to the right of the peak. walk forward until the slope begins decreasing
-    uint8_t  neg_slope_ind = peak_ind;
-    uint16_t loop_limit    = NUM_AUTOCORR_LAGS-1;
-    while ((autocorr_buff[neg_slope_ind+1] - autocorr_buff[neg_slope_ind] < 0) && (neg_slope_ind < loop_limit)) {
-        *neg_slope_count = *neg_slope_count + 1;
-        neg_slope_ind    = neg_slope_ind + 1;
-    }
-    
-    //get the delta amplitude between peak and right trough
-    *delta_amplitude_right = autocorr_buff[peak_ind] - autocorr_buff[neg_slope_ind];
-    
-    //next look to the left of the peak. walk backward until the slope begins increasing
-    uint8_t pos_slope_ind = peak_ind;
-    loop_limit    = 0;
-    while ((autocorr_buff[pos_slope_ind] - autocorr_buff[pos_slope_ind-1] > 0) && (pos_slope_ind > loop_limit)) {
-        *pos_slope_count = *pos_slope_count + 1;
-        pos_slope_ind    = pos_slope_ind - 1;
-    }
-    
-    //get the delta amplitude between the peak and the left trough
-    *delta_amplitude_left = autocorr_buff[peak_ind] - autocorr_buff[pos_slope_ind];
-}
-
-
-//use the original autocorrelation signal to hone in on the
-//exact peak index. this corresponds to the point where the points to the
-//left and right are less than the current point
-static uint8_t get_precise_peakind(int64_t *autocorr_buff, uint8_t peak_ind) {
-    uint8_t loop_limit = 0;
-    if ((autocorr_buff[peak_ind] > autocorr_buff[peak_ind-1]) && (autocorr_buff[peak_ind] > autocorr_buff[peak_ind+1])) {
-        //peak_ind is perfectly set at the peak. nothing to do
-    }
-    else if ((autocorr_buff[peak_ind] > autocorr_buff[peak_ind+1]) && (autocorr_buff[peak_ind] < autocorr_buff[peak_ind-1])) {
-        //peak is to the left. keep moving in that direction
-        loop_limit = 0;
-        while ((autocorr_buff[peak_ind] > autocorr_buff[peak_ind+1]) && (autocorr_buff[peak_ind] < autocorr_buff[peak_ind-1]) && (loop_limit < 10)) {
-            peak_ind = peak_ind - 1;
-            loop_limit++;
-        }
-    }
-    else {
-        //peak is to the right. keep moving in that direction
-        loop_limit = 0;
-        while ((autocorr_buff[peak_ind] > autocorr_buff[peak_ind-1]) && (autocorr_buff[peak_ind] < autocorr_buff[peak_ind+1]) && (loop_limit < 10)) {
-            peak_ind = peak_ind + 1;
-            loop_limit++;
-        }
-    }
-    return peak_ind;
-}
-
-
-//calculate deriviative via FIR filter
-static void derivative(int64_t *autocorr_buff, int64_t *deriv) {
-    
-    uint8_t n          = 0;
-    uint8_t i          = 0;
-    int64_t temp_deriv = 0;
-    for (n = 0; n < NUM_AUTOCORR_LAGS; n++) {
-        temp_deriv = 0;
-        for (i = 0; i < DERIV_FILT_LEN; i++) {
-            if (n-i >= 0) {     //handle the case when n < filter length
-                temp_deriv += deriv_coeffs[i]*autocorr_buff[n-i];
-            }
-        }
-        deriv[n] = temp_deriv;
-    }
-}
-
-
-//autocorrelation function
-//this takes a lot of computation. there are efficient implementations, but this is more intuitive
-static void autocorr(int32_t *lpf, int64_t *autocorr_buff) {
-    
-    uint8_t lag;
-    uint16_t i;
-    int64_t temp_ac;
-    for (lag = 0; lag < NUM_AUTOCORR_LAGS; lag++) {
-        temp_ac = 0;
-        for (i = 0; i < COUNT_STEPS_NUM_TUPLES-lag; i++) {
-            temp_ac += (int64_t)lpf[i]*(int64_t)lpf[i+lag];
-        }
-        autocorr_buff[lag] = temp_ac;
-    }
-}
-
-
-//calculate and remove the mean
-static void remove_mean(int32_t *lpf) {
-    
-    int32_t sum = 0;
-    uint16_t i;
-    for (i = 0; i < COUNT_STEPS_NUM_TUPLES; i++) {
-        sum += lpf[i];
-    }
-    sum = sum/(COUNT_STEPS_NUM_TUPLES);
-    
-    for (i = 0; i < COUNT_STEPS_NUM_TUPLES; i++) {
-        lpf[i] = lpf[i] - sum;
-    }
-}
-
-
-//FIR low pass filter
-static void lowpassfilt(uint8_t *mag_sqrt, int32_t *lpf) {
-    
-    uint16_t n;
-    uint8_t i;
-    int32_t temp_lpf;
-    for (n = 0; n < COUNT_STEPS_NUM_TUPLES; n++) {
-        temp_lpf = 0;
-        for (i = 0; i < LPF_FILT_LEN; i++) {
-            if (n-i >= 0) {     //handle the case when n < filter length
-                temp_lpf += (int32_t)lpf_coeffs[i]*(int32_t)mag_sqrt[n-i];
-            }
-        }
-        lpf[n] = temp_lpf;
-    }
-}
-
-static bool allzeroes(uint8_t *mag_sqrt) {
-    uint16_t n;
-    bool all_zero = true;
-    for (n = 0; n < COUNT_STEPS_NUM_TUPLES; n++) {
-        if (mag_sqrt[n] != 0) {
-            all_zero = false;
-            break;
-        }
-    }
-    return all_zero;
-}
-
-//algorithm interface
-uint8_t count_steps(uint8_t *mag_sqrt) {
-    //assume data is in the format data = [approx_l2_norm(x1,y1,z1),approx_l2_norm(x2,y2,z2)...etc]
-    uint16_t i;
-
-    //check if all of the values are zero (which means the data is empty)
-    if (allzeroes(mag_sqrt)) return 0;
-    
-    //apply low pass filter to mag_sqrt, result is stored in lpf
-    lowpassfilt(mag_sqrt, lpf);
-    
-    //remove mean from lpf, store result in lpf
-    remove_mean(lpf);
-    
-    //do the autocorrelation on the lpf buffer, store the result in autocorr_buff
-    autocorr(lpf, autocorr_buff);
-    
-    //get the derivative of the autocorr_buff, store in deriv
-    derivative(autocorr_buff, deriv);
-    
-    //look for first zero crossing where derivative goes from positive to negative. that
-    //corresponds to the first positive peak in the autocorrelation. look at two samples
-    //instead of just one to maybe reduce the chances of getting tricked by noise
-    uint8_t peak_ind = 0;
-    //start index is set to 8 lags, which corresponds to a walking rate of 2.5Hz @20Hz sampling rate. its probably
-    //running if its faster than this
-    for (i = 8; i < NUM_AUTOCORR_LAGS; i++) {
-        if ((deriv[i] > 0) && (deriv[i-1] > 0) && (deriv[i-2] < 0) && (deriv[i-3] < 0)) {
-            peak_ind = i-1;
-            break;
-        }
-    }
-    
-    //hone in on the exact peak index
-    peak_ind = get_precise_peakind(autocorr_buff, peak_ind);
-    //printf("peak ind: %i\n", peak_ind);
-    
-    //get autocorrelation peak stats
-    uint8_t neg_slope_count = 0;
-    int64_t delta_amplitude_right = 0;
-    uint8_t pos_slope_count = 0;
-    int64_t delta_amplitude_left = 0;
-    get_autocorr_peak_stats(autocorr_buff, &neg_slope_count, &delta_amplitude_right, &pos_slope_count, &delta_amplitude_left, peak_ind);
-    
-    //now check the conditions to see if it was a real peak or not, and if so, calculate the number of steps
-    uint8_t num_steps = 0;
-    if ((pos_slope_count > AUTOCORR_MIN_HALF_LEN) && (neg_slope_count > AUTOCORR_MIN_HALF_LEN) && (delta_amplitude_right > AUTOCORR_DELTA_AMPLITUDE_THRESH) && (delta_amplitude_left > AUTOCORR_DELTA_AMPLITUDE_THRESH)) {
-        //the period is peak_ind/COUNT_STEPS_SAMPLING_RATE seconds. that corresponds to a frequency of 1/period
-        //with the frequency known, and the number of seconds is 4 seconds, you can then find out the number of steps
-        num_steps = (COUNT_STEPS_NUM_TUPLES)/peak_ind;
-    } else {
-        //not a valid autocorrelation peak
-        num_steps = 0;
-    }
-    
-    //printf("num steps: %i\n", num_steps);
-    return num_steps;
-}
 
 uint8_t count_steps_simple(lis2dw_fifo_t *fifo_data) {
     uint8_t new_steps = 0;
@@ -298,43 +79,8 @@ uint8_t count_steps_simple(lis2dw_fifo_t *fifo_data) {
 }
 
 
-/*
- * This file is part of Espruino, a JavaScript interpreter for Microcontrollers
- *
- * Copyright (C) 2021 Gordon Williams <gw@pur3.co.uk>
- *
- * This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/.
- *
- * ----------------------------------------------------------------------------
- * Step Counter
- * ----------------------------------------------------------------------------
- */
-
-#include <stdlib.h>
-#include <stdint.h>
-#include <stdbool.h>
-
-// a1bc34f9a9f5c54b9d68c3c26e973dba195e2105   HughB-walk-1834.csv  1446
-// oxford filter                                                   1584
-// peak detection                                                  1752
-// state machine                                                   1751
 
 // STEPCOUNT_CONFIGURABLE is for use with https://github.com/gfwilliams/step-count
-// to test/configure the step counter offline
-
-/*
-
-==========================================================
-FIR filter designed with http://t-filter.engineerjs.com/
-
-AccelFilter_get modified to return 8 bits of fractional
-data.
-==========================================================
-
-*/
-
 #define ACCELFILTER_TAP_NUM 7
 
 typedef struct {
@@ -464,7 +210,7 @@ bool gate_open = false;        // start closed
 // ===============================================================
 
 // Init step count
-void stepcount_init() {
+void count_steps_espruino_init() {
   AccelFilter_init(&accelFilter);
   DCFilter_sample_avg_total = 8192*NSAMPLE;
   accFiltered = 0;
@@ -540,7 +286,7 @@ static int stepcount_had_step() {
 }
 
 // do calculations
-int stepcount_new(uint32_t accMag) {
+uint8_t count_steps_espruino_sample(uint32_t accMag) {
   // scale to fit and clip
   //int v = (accMag-8192)>>5;
   int v = DCFilter(accMag)>>5;
@@ -589,12 +335,12 @@ int stepcount_new(uint32_t accMag) {
 }
 
 
-int stepcount_new_from_fifo(lis2dw_fifo_t *fifo_data) {
+uint8_t count_steps_espruino(lis2dw_fifo_t *fifo_data) {
     uint8_t new_steps = 0;
 
     for (uint8_t i = 0; i < fifo_data->count; i++) {
         uint32_t magnitude = count_steps_approx_l2_norm(fifo_data->readings[i]);
-        new_steps += stepcount_new(magnitude);
+        new_steps += count_steps_espruino_sample(magnitude);
     }
 
     if (new_steps > MAX_SIMPLE_STEPS) new_steps = MAX_SIMPLE_STEPS;

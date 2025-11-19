39c4950857d68b9d9bcb5e4fe5d8f8eba101a352
David Volovskiy <devolov@gmail.com>
2025-11-19 08:44:12 -0500

    Added prints for LIS2DW interrupts

--- movement.c
+++ movement.c
@@ -338,12 +338,21 @@ static void _movement_renew_top_of_minute_alarm(void) {
     movement_volatile_state.schedule_next_comp = true;
 }
 
-#define PRINT_LIS2DUX_EVENTS false
+#define PRINT_LIS_EVENTS false
 static uint32_t _movement_get_accelerometer_events() {
     uint32_t accelerometer_events = 0;
 #ifdef I2C_SERCOM
     if (movement_state.has_lis2dw) {
         uint8_t int_src = lis2dw_get_interrupt_source();
+#if PRINT_LIS_EVENTS
+        printf("_movement_get_accelerometer_events\r\n");
+        if (int_src & LIS2DW_REG_ALL_INT_SRC_SLEEP_CHANGE_IA)  printf("Sleep Change IA\r\n");
+        if (int_src & LIS2DW_REG_ALL_INT_SRC_6D_IA)            printf("6D IA\r\n");
+        if (int_src & LIS2DW_REG_ALL_INT_SRC_SINGLE_TAP)       printf("Single Tap\r\n");
+        if (int_src & LIS2DW_REG_ALL_INT_SRC_DOUBLE_TAP)       printf("Double Tap\r\n");
+        if (int_src & LIS2DW_REG_ALL_INT_SRC_WU_IA)            printf("Wake Up\r\n");
+        if (int_src & LIS2DW_REG_ALL_INT_SRC_FF_IA)            printf("Free Fall\r\n");
+#endif
         if (int_src & LIS2DW_REG_ALL_INT_SRC_DOUBLE_TAP) {
             accelerometer_events |= 1 << EVENT_DOUBLE_TAP;
             printf("Double tap!\r\n");
@@ -356,8 +365,8 @@ static uint32_t _movement_get_accelerometer_events() {
     else if (movement_state.has_lis2dux) {
         lis2dux12_all_sources_t int_src;
         lis2dux12_all_sources_get(&dev_ctx, &int_src);
-#if PRINT_LIS2DUX_EVENTS
-        printf("cb_accelerometer_lis2dux_event\r\n");
+#if PRINT_LIS_EVENTS
+        printf("_movement_get_accelerometer_events\r\n");
         if (int_src.drdy)             printf("drdy:             %d\r\n", int_src.drdy);
         if (int_src.free_fall)       printf("free_fall:        %d\r\n", int_src.free_fall);
         if (int_src.wake_up)         printf("wake_up:          %d\r\n", int_src.wake_up);

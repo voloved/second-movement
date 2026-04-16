3580d907ddb51d218a2a8ab3eb69c34afad56a47
David Volovskiy <devolov@gmail.com>
2026-04-16 07:59:41 -0400

    Added XLCD logic

--- watch-library/hardware/watch/watch_slcd.c
+++ watch-library/hardware/watch/watch_slcd.c
@@ -237,12 +237,12 @@ void watch_enable_display(void) {
 
     if (_installed_display == WATCH_LCD_TYPE_CUSTOM) {
         // Custom LCD: 1/3 bias, 1/4 duty with a frame rate of 32 Hz
-        slcd_init(LCD_PIN_ENABLE, SLCD_BIAS_THIRD, SLCD_DUTY_4_COMMON, SLCD_CLOCKSOURCE_XOSC, SLCD_PRESCALER_DIV64, SLCD_CLOCKDIV_4);
+        slcd_init(LCD_PIN_ENABLE, SLCD_BIAS_THIRD, SLCD_DUTY_4_COMMON, SLCD_CLOCKSOURCE_XOSC, SLCD_PRESCALER_DIV64, SLCD_CLOCKDIV_4, SLCD_XVLCD_INTERNAL);
         // exact frame rate is: 32768 / (4 * 64 * 4) ≈ 32 Hz
         _slcd_framerate = 32;
     } else {
         // Original famous Casio LCD: 1/3 bias, 1/3 duty with a frame rate of ~34 Hz
-        slcd_init(LCD_PIN_ENABLE, SLCD_BIAS_THIRD, SLCD_DUTY_3_COMMON, SLCD_CLOCKSOURCE_XOSC, SLCD_PRESCALER_DIV64, SLCD_CLOCKDIV_5);
+        slcd_init(LCD_PIN_ENABLE, SLCD_BIAS_THIRD, SLCD_DUTY_3_COMMON, SLCD_CLOCKSOURCE_XOSC, SLCD_PRESCALER_DIV64, SLCD_CLOCKDIV_5, SLCD_XVLCD_INTERNAL);
         // exact frame rate is: 32768 / (3 * 64 * 5) ≈ 34.13 Hz
         _slcd_framerate = 34;
     }

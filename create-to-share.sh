4cb2ad017e5a9fbdb31fc554c1d9c4021102f934
David Volovskiy <devolov@gmail.com>
2026-05-24 08:09:06 -0400

    Added github action to add release on a tag

--- create-to-share.sh
+++ create-to-share.sh
@@ -12,6 +12,14 @@ else
     echo "BUILDING TO SHARE"
 fi
 
+TAG_CLEAN=""
+if [ -n "$GITHUB_REF_NAME" ]; then
+    TAG="${GITHUB_REF_NAME#share-}"
+    TAG_CLEAN="-${TAG//./}"
+fi
+
+GIT_HASH=$(git rev-parse --short=6 HEAD)
+
 current_branch=$(git rev-parse --abbrev-ref HEAD)
 
 # Check for detached HEAD
@@ -27,7 +35,6 @@ rm -rf "$RELEASE_DIR"
 build_and_move() {
     local board="$1"
     local display="$2"
-    local branch="$3"
 
     make clean
     if $SHARE; then
@@ -35,20 +42,21 @@ build_and_move() {
     else
         make BOARD="$board" DISPLAY="$display"
     fi
-    mkdir -p "$RELEASE_DIR/$branch"
-    mv build/firmware.uf2 "$RELEASE_DIR/$branch/firmware-${board}-${display}.uf2"
+    mkdir -p "$RELEASE_DIR"
+    mv build/firmware.uf2 "$RELEASE_DIR/firmware-${board}-${display}${TAG_CLEAN}-${GIT_HASH}.uf2"
 }
 
 build_all_for_branch() {
     local branch="$1"
     git checkout "$branch"
-    build_and_move sensorwatch_pro classic "$branch"
-    build_and_move sensorwatch_pro custom "$branch"
-    build_and_move sensorwatch_red custom "$branch"
-    build_and_move sensorwatch_red classic "$branch"
+    build_and_move sensorwatch_jolt jolt
+    build_and_move sensorwatch_pro classic
+    build_and_move sensorwatch_pro custom
+    build_and_move sensorwatch_red custom
+    build_and_move sensorwatch_red classic
 }
 
-build_all_for_branch devolov
+build_all_for_branch jolt
 
 if [ -n "$return_to_branch" ]; then
     git checkout "$return_to_branch"
